349248070eb3841cd7ec5d413a0d1a7699e6b2b8
yanaramli <yanaramli2208@gmail.com>
2019-12-03 22:53:35 +0800

    Updates

---
+++ precache-manifest.59c557c92dea90201bf69d7869e355cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4088247c351d14b3bb536f32a3eed984",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "7a83927258aa801f2392",
+    "url": "/reactjs/static/css/2.17be12a0.chunk.css"
+  },
+  {
+    "revision": "7a83927258aa801f2392",
+    "url": "/reactjs/static/js/2.feb80013.chunk.js"
+  },
+  {
+    "revision": "2c28678921eed1e2519a",
+    "url": "/reactjs/static/js/main.076434e5.chunk.js"
+  },
+  {
+    "revision": "471e0943c565b08d3bb5",
+    "url": "/reactjs/static/js/runtime-main.f2cb4a9e.js"
+  }
+]);
